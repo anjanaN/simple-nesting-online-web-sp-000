a723571fda07ebd7f9e51a7bbfdee28f99508233
Anjana Nair <anju740@gmail.com>
2019-09-06 14:47:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,8 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = [:known_for,:languages]
-#programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-#programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
 puts programmer_hash
 end
 adding_matz
